3bb7285d30c3e68b7eebe5b1125460ecaf0eaba5
ashen iranga hewarathna <asheniranga@gmail.com>
2022-01-07 18:33:54 +0530

    report generator

--- mobile application/app/src/main/java/com/example/ildd/MainActivity.kt
+++ mobile application/app/src/main/java/com/example/ildd/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.ildd
 import android.content.Intent
 import android.content.res.AssetManager
 import android.graphics.Bitmap
-import android.graphics.Path
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -20,14 +19,19 @@ import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import java.util.*
+import kotlin.Comparator
 import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
 
     private val IMAGE_MEAN = 0
     private val IMAGE_STD = 255.0f
+    private val MAX_RESULTS = 4
+    private val THRESHOLD = 0.4f
     lateinit var bitmap: Bitmap
     lateinit var imageView: ImageView
+    var labelList = loadLabelList(application.assets, "labels.txt")
 
     // output creation function
     data class Recognition(
@@ -38,6 +42,11 @@ class MainActivity : AppCompatActivity() {
         override fun toString(): String {
             return "Title = $title, Confidence = $confidence"
         }
+    }
+
+    private fun loadLabelList(assetManager: AssetManager, labelPath: String): List<String> {
+        return assetManager.open(labelPath).bufferedReader().useLines { it.toList() }
+
     }
 
     // convert bitmap to float array and scale to 0-1 range
@@ -68,9 +77,6 @@ class MainActivity : AppCompatActivity() {
 
         var load_img: Button = findViewById(R.id.btn_load_image)
         var tv: TextView = findViewById(R.id.tv_output)
-        val assetFile = "labels.txt"
-        val inputString =
-            application.assets.open(assetFile).bufferedReader().useLines { it.toList() }
 
         // fire actions when click on load image button
         load_img.setOnClickListener(View.OnClickListener {
@@ -101,6 +107,8 @@ class MainActivity : AppCompatActivity() {
             val outputs = model.process(inputFeature0)
             val outputFeature0 = outputs.outputFeature0AsTensorBuffer
 
+            bestPreds = getSortedResult()
+
             tv.setText()
 
             // Releases model resources if no longer used.
@@ -121,15 +129,40 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // get sorted results to show
     private fun getSortedResult(labelProbArray: Array<FloatArray>): List<MainActivity.Recognition> {
         Log.d(
             "Classifier",
             "List Size:(%d,%d,%d)".format(
-                labelProbArray.size,
-                labelProbArray[0].size,
-                labelList.size
+                labelProbArray.size, labelProbArray[0].size, labelList.size
             )
         )
+
+        val pq = PriorityQueue(
+            MAX_RESULTS,
+            Comparator<MainActivity.Recognition> { (_, _, confidence1), (_, _, confidence2) ->
+                java.lang.Float.compare(confidence1, confidence2) * -1
+            })
+
+        for (i in labelList.indices) {
+            val confidence = labelProbArray[0][i]
+            if (confidence >= THRESHOLD) {
+                pq.add(
+                    MainActivity.Recognition(
+                        "" + i,
+                        if (labelList.size > i) labelList[i] else "Unknown", confidence
+                    )
+                )
+            }
+        }
+
+        val recognitions = ArrayList<MainActivity.Recognition>()
+        val recognitionsSize = Math.min(pq.size, MAX_RESULTS)
+        for (i in 0 until recognitionsSize) {
+            recognitions.add(pq.poll())
+        }
+
+        return recognitions
     }
 
 }
